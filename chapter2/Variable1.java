1d970336b1cab2eb1c0f566fae56cafe94ffebc6
gjaeu0112 <86722530+gjaeu0112@users.noreply.github.com>
2022-07-22 22:33:25 +0900

    First commit

---
+++ chapter2/Variable1.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class Variable1 {
+	public static void main(String[] args) {
+		//변수를 선언하고 값을 대입하고 출력하는 프로그램
+		int level;		//정수형 변수 level을 선언
+		level = 10;		//level 변수에 값 10을 대입
+		System.out.println(level);	//level 값 출력
+		
+		int age;		//정수형 변수 age를 선언
+		age = 21;		//age 변수에 값 21을 대입
+		System.out.println(age);	//age 값 출력
+	}
+}
